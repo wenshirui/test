38319544aecee24521fd3d74e3a6fa2e462ba7b4
win <Catcat_win@163.com>
2019-08-11 15:51:27 +0800

    添加get&&set方法

--- eureka-server/src/main/java/com/guqi/TomCat2.java
+++ eureka-server/src/main/java/com/guqi/TomCat2.java
@@ -1,7 +1,25 @@
 package com.guqi;
 
-public class TomCat2 {
+import java.io.Serializable;
+
+
+public class TomCat2  implements Serializable{
     private  long id;
     private  String name;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
