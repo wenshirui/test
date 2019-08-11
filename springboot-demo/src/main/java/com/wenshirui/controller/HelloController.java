981a3c8eb055027f27fa9d4f11d70cec7acb2b17
win <Catcat_win@163.com>
2019-08-11 17:19:12 +0800

    springboot工程里边,写一些东西

---
+++ springboot-demo/src/main/java/com/wenshirui/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.wenshirui.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "进来了吗?";
+    }
+}
